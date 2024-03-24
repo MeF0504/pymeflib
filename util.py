fa44147e74a1639f77358b0883b95a6576209703
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-03-24 16:46:46 +0900

    Update the document and argument of chk_cmd.

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import os
 import os.path as op
 import platform
 from logging import getLogger, NullHandler, Logger
-from typing import Optional
+from typing import Optional, Union
 
 __logger = getLogger(__name__)
 __null_hdlr = NullHandler()
@@ -29,7 +29,8 @@ def mkdir(path: str) -> None:
         # os.chmod(path,0755)
 
 
-def chk_cmd(cmd: str, logger: Optional[Logger] = None) -> bool:
+def chk_cmd(cmd: str, return_path: bool = False,
+            logger: Optional[Logger] = None) -> Union[bool, Optional[str]]:
     """
     check the command exists and is executable.
 
@@ -38,21 +39,36 @@ def chk_cmd(cmd: str, logger: Optional[Logger] = None) -> bool:
     cmd: str
         the checked command.
         both the command itself or full path of that command is acceptable.
+    return_path: bool
+        if True, return the path of `cmd`.
+        default: False
+    logger: None or Logger
+        specify the logger. if None, nothing is logged.
+        default: None
 
     Returns
     -------
-    None
+    if return_path is True, the string of path is returned.
+    if `cmd` does not exist or is not executable, return None.
+    if return_path is False, return True if `cmd` exists and is executable
+    and otherwise return False.
     """
     if logger is None:
         logger = __logger
     full_path = os.path.expanduser(os.path.expandvars(cmd))
     if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
         logger.info(f'find {full_path}')
-        return True
+        if return_path:
+            return full_path
+        else:
+            return True
 
     if 'PATH' not in os.environ:
         logger.warning("PATH isn't found in environment values.")
-        return False
+        if return_path:
+            return None
+        else:
+            return False
 
     if platform.uname()[0] == 'Windows':
         cmd = '{}.exe'.format(cmd)
@@ -60,6 +76,12 @@ def chk_cmd(cmd: str, logger: Optional[Logger] = None) -> bool:
         cmd_path = op.join(path, cmd)
         if op.isfile(cmd_path) and os.access(cmd_path, os.X_OK):
             logger.info(f'find {cmd} ... {cmd_path}')
-            return True
+            if return_path:
+                return cmd_path
+            else:
+                return True
     logger.info(f'command {cmd} is not found.')
-    return False
+    if return_path:
+        return None
+    else:
+        return False
